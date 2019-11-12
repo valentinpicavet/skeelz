5e16576ec460138f4b807a92454b87cd609ded28
arthur <arthur.robert18@hotmail.fr>
2019-11-12 17:24:34 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ src/model/SuperUser.java
@@ -0,0 +1,56 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SuperUser {
+
+	private Long id;
+	private int version;
+	private String nom;
+	private String prenom;
+	
+	private List<Entreprise> entreprises = new ArrayList<Entreprise>();
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public int getVersion() {
+		return version;
+	}
+
+	public void setVersion(int version) {
+		this.version = version;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public List<Entreprise> getEntreprises() {
+		return entreprises;
+	}
+
+	public void setEntreprises(List<Entreprise> entreprises) {
+		this.entreprises = entreprises;
+	}
+	
+	
+}
