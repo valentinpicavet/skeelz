0fafcd5849bac91c539e35a8241d6e624c8071d2
Vince <vmaridet@gmail.com>
2019-11-18 17:26:02 +0100

    query cours

--- skeelz-data-jpa/src/main/java/repository/ICoursRepository.java
+++ skeelz-data-jpa/src/main/java/repository/ICoursRepository.java
@@ -1,10 +1,14 @@
 package repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import skeelz.modele.Cours;
 
 public interface ICoursRepository extends JpaRepository<Cours, Long> {
-	
-
+	@Query("from cours c left join fetch c.module m left join fetch m.chapitre ch where c = :cours")
+	List<Cours> findAllByCours(@Param("cours") Cours cours);
 }
