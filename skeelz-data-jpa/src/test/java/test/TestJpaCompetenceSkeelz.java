00a90648fd6ef906034e63d482397703025bf62d
arthur <arthur.robert18@hotmail.fr>
2019-11-18 16:31:53 +0100

    test CompetencePersonne

--- skeelz-data-jpa/src/test/java/test/TestJpaCompetenceSkeelz.java
+++ skeelz-data-jpa/src/test/java/test/TestJpaCompetenceSkeelz.java
@@ -1,34 +1,59 @@
 package test;
 
-import Singleton.Singleton;
-import repository.ICompetenceSkeelzRepository;
-import skeelz.modele.CompetenceSkeelz;
+import java.util.Optional;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import repository.ICompetenceRepository;
+import repository.ICompetenceSkeelzRepository;
+import skeelz.modele.Competence;
+import skeelz.modele.CompetenceSkeelz;
+import skeelz.modele.Ponderation;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/application-context.xml")
 public class TestJpaCompetenceSkeelz {
 
-	public static void main(String[] args) {
-		
-		ICompetenceSkeelzRepository compsRepo = Singleton.getInstance().getCompetenceSkeelzRepo();
-		
-		int startNumber = compsRepo.findAll().size();
-		
-		CompetenceSkeelz comps = new CompetenceSkeelz();
-		
-		
+	@Autowired
+	private ICompetenceSkeelzRepository competenceSkeelzRepo;
+	@Autowired
+	private ICompetenceRepository competenceRepo;
+	
+	@Test
+	public void testCompetenceSkeelz() {
 		
 		
-		comps = compsRepo.save(comps);
+		int startNumber = competenceSkeelzRepo.findAll().size();
+		
+		CompetenceSkeelz compSk = new CompetenceSkeelz();
+		Competence maCompetence = new Competence();
+		maCompetence.setIntitule("java");
+		maCompetence.setPonderation(Ponderation.DIX);
+		
+		maCompetence = competenceRepo.save(maCompetence);
+		compSk.setCompetence(maCompetence);
+		
+		compSk = competenceSkeelzRepo.save(compSk);
 		
 		
 		
-		comps = compsRepo.find(comps.getId());
+		Optional<CompetenceSkeelz> compSkFind = competenceSkeelzRepo.findById(compSk.getId());
+		Assert.assertEquals("java", compSkFind.get().getCompetence().getIntitule());
 		
+		int middleNumber = competenceSkeelzRepo.findAll().size();
+		Assert.assertEquals(1, (middleNumber - startNumber));
 		
-		int middleNumber = compsRepo.findAll().size();
-		System.out.println(middleNumber - startNumber);
+		competenceSkeelzRepo.delete(compSk);
 		
-		compsRepo.delete(comps);
+		int finalNumber = competenceSkeelzRepo.findAll().size();
 		
+		Assert.assertEquals(0, finalNumber - startNumber);
+
 	}
 
 }
