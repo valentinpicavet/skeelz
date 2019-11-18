1b5b0a983287b61b0796768c40d26bf163478e02
Vince <vmaridet@gmail.com>
2019-11-18 17:55:20 +0100

    debut requete chapitre

--- skeelz-data-jpa/src/main/java/repository/IChapitreRepository.java
+++ skeelz-data-jpa/src/main/java/repository/IChapitreRepository.java
@@ -1,9 +1,18 @@
 package repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import skeelz.modele.Chapitre;
 
 public interface IChapitreRepository extends JpaRepository<Chapitre, Long> {
 
+//	@Query("from chapitre ch where ch.module.id = :id and ch.agencement = :agencementplus")
+//	List<Chapitre> findAllByChapitre (@Param("id") Long chapitreId, @Param("agencementplus") Integer agencementChapitre);
+//	
+//	@Query("from chapitre ch where ch.module.id = :id and ch.agencement = :agencementmoins-1")
+//	List<Chapitre> findAllByChapitre (@Param("id") Long chapitreId, @Param("agencementmoins") Integer agencementChapitre);
 }
