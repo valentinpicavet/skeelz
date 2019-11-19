265de719a2bed1b65eaecec7c9ea2a807638a482
Damien <lignac.damien@gmail.com>
2019-11-19 11:16:47 +0100

    Ecran cours query

--- skeelz-data-jpa/src/main/java/repository/ICoursRepository.java
+++ skeelz-data-jpa/src/main/java/repository/ICoursRepository.java
@@ -14,24 +14,19 @@ import skeelz.modele.Difficulte;
 public interface ICoursRepository extends JpaRepository<Cours, Long> {
 	
 
-//	@Query("from cours c left join fetch c.module m left join fetch m.chapitre ch where c = :cours")
-//	List<Cours> findAllByCours(@Param("cours") Cours cours);
-//	
+
 //	@Query ("from cours c where c.difficulte = :difficulte ")
 	List<Cours> findAllCoursByDifficulte(Difficulte difficulte);
-//	
+	
 	@Query ("select cc.cours from CoursCompetence cc order by cc.competence ")
 	List<Cours> findAllCoursByCompetence();
 	
 	@Query ("select cc.cours from CoursCompetence cc join  cc.competence c join  c.competenceSkeelz cs order by cs.skeelz ")
 	List<Cours> findAllCoursBySkeelz();
 	
-//	
-//	@Query ("from cours c order by c.duree")
-//	List<Cours> findAllCoursByDuree ();
-//	
-//	@Query ("from cours")
-//	List<Cours> findAllCoursByDifficulte(@Param("difficulte")Difficulte difficulte);
+	@Query ("from Cours c order by c.duree")
+	List<Cours> findAllCoursByDuree ();
+
 	
 	
 }
