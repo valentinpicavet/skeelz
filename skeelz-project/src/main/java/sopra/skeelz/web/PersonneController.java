7c3998b7eee86688cf71765fb2e5f25c25574339
Damien <lignac.damien@gmail.com>
2019-11-27 16:16:40 +0100

    C C C C C l'hymen

---
+++ skeelz-project/src/main/java/sopra/skeelz/web/PersonneController.java
@@ -0,0 +1,60 @@
+package sopra.skeelz.web;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.annotation.JsonView;
+
+import sopra.skeelz.model.Personne;
+import sopra.skeelz.model.Views;
+import sopra.skeelz.repository.IPersonneRepository;
+
+
+@RestController
+@RequestMapping("/personne")
+public class PersonneController {
+	@Autowired
+	private IPersonneRepository personneRepo;
+
+	@GetMapping("")
+	@JsonView(Views.ViewCommon.class)
+	public List<Personne> list() {
+		List<Personne> personnes = personneRepo.findAll();
+
+		return personnes;
+	}
+
+	@GetMapping("/{id}")
+	@JsonView(Views.ViewCommon.class)
+	public Personne find(@PathVariable Long id) {
+		Personne personne = personneRepo.findById(id).get();
+
+		return personne;
+	}
+
+	@PostMapping("")
+	public Personne create(@RequestBody Personne personne) {
+		return personneRepo.save(personne);
+	}
+
+	@PutMapping("/{id}")
+	public Personne update(@RequestBody Personne personne, @PathVariable Long id) {
+		return personneRepo.save(personne);
+	}
+
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		personneRepo.deleteById(id);
+	}
+}
+
+
---
+++ skeelz-project/src/main/java/sopra/skeelz/web/QCMPersonneController.java
@@ -0,0 +1,59 @@
+package sopra.skeelz.web;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.annotation.JsonView;
+
+import sopra.skeelz.model.QCMPersonne;
+import sopra.skeelz.model.Views;
+import sopra.skeelz.repository.IQCMPersonneRepository;
+
+
+@RestController
+@RequestMapping("/qcmPersonne")
+public class QCMPersonneController {
+	@Autowired
+	private IQCMPersonneRepository qcmPersonneRepo;
+
+	@GetMapping("")
+	@JsonView(Views.ViewCommon.class)
+	public List<QCMPersonne> list() {
+		List<QCMPersonne> qcmPersonnes = qcmPersonneRepo.findAll();
+
+		return qcmPersonnes;
+	}
+
+	@GetMapping("/{id}")
+	@JsonView(Views.ViewCommon.class)
+	public QCMPersonne find(@PathVariable Long id) {
+		QCMPersonne qcmPersonne = qcmPersonneRepo.findById(id).get();
+		return qcmPersonne;
+	}
+
+	@PostMapping("")
+	public QCMPersonne create(@RequestBody QCMPersonne qcmPersonne) {
+		return qcmPersonneRepo.save(qcmPersonne);
+	}
+
+	@PutMapping("/{id}")
+	public QCMPersonne update(@RequestBody QCMPersonne qcmPersonne, @PathVariable Long id) {
+		return qcmPersonneRepo.save(qcmPersonne);
+	}
+
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		qcmPersonneRepo.deleteById(id);
+	}
+}
+
+
